34880185913665fe608f4d09c38d3d6763ef6c40
Erick Berry <Erick@Briaunas-Air.hsd1.wa.comcast.net>
2016-03-31 16:01:18 -0700

    fully functional

--- apps/surveys/views.py
+++ apps/surveys/views.py
@@ -2,8 +2,21 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
+
+
 	return render(request,'surveys/index.html')
+
 def process_form(request):
+	try:
+		request.session['count'] += 1
+	except:
+		request.session['count'] = 1
+	request.session['info'] =  request.POST
 	return redirect('/result')
+
 def result(request):
-	return render(request, 'surveys/result.html')
+	context = {
+		'count': request.session['count'],
+		'info': request.session['info']
+	}
+	return render(request, 'surveys/result.html', context)
